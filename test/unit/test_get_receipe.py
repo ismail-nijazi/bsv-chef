5a7ce394838177ee840bac0e9d5dee85ed096b33
ismail-nijazi <ismail.nijazi1@gmail.com>
2023-05-22 11:30:56 +0200

    ...

--- test/unit/test_get_receipe.py
+++ test/unit/test_get_receipe.py
@@ -30,14 +30,13 @@ def test_receipe_not_available_readiness_above_0_1(mock_calculate_readiness, rec
 def test_receipe_not_available_readiness_below_0_1(mock_calculate_readiness, receipe_controller, test_data):
     receipe, available_items, diet = test_data
     receipe['diets'] = ['vegan']
-    mock_calculate_readiness.return_value = 0.05  # Mock the calculate_readiness function
+    mock_calculate_readiness.return_value = 0.05  
     result = receipe_controller.get_receipe_readiness(receipe, available_items, diet)
     assert result is None
 
 @pytest.mark.unit
 @patch('src.controllers.receipecontroller.calculate_readiness')
 def test_receipe_available_readiness_above_0_1(mock_calculate_readiness, receipe_controller, test_data):
-    # Test case 3: receipe is available and readiness is above 0.1
     receipe, available_items, diet = test_data
     mock_calculate_readiness.return_value = 0.8  
     result = receipe_controller.get_receipe_readiness(receipe, available_items, diet)
